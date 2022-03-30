ba8e5478ae1942445d73e66c883eb7fde2ac7e06
Herve BOISGONTIER <hboisgontier@eni-ecole.fr>
2022-03-30 09:48:13 +0200

    TP5

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Wish;
+use App\Repository\WishRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,17 +12,49 @@ use Symfony\Component\Routing\Annotation\Route;
 class WishController extends AbstractController
 {
     #[Route('/wish', name: 'app_wish_list')]
-    public function list(): Response
+    public function list(WishRepository $repository): Response
     {
-        return $this->render('wish/list.html.twig', [
-            'controller_name' => 'WishController',
+        $wishes = $repository->findBy(['isPublished'=>true], ['dateCreated'=>'DESC']);
+        return $this->render('wish/list.html.twig', compact('wishes'));
+    }
+    #[Route('/wish/details/{id}', name: 'app_wish_details', requirements: ['id'=>'\d+'])]
+    public function details($id, WishRepository $repository): Response
+    {
+        $wish = $repository->find($id);
+        if(!$wish)
+            throw $this->createNotFoundException();
+        return $this->render('wish/details.html.twig', [
+            'wish' => $wish,
         ]);
     }
-    #[Route('/wish/details/{id}', name: 'app_wish_deatils', requirements: ['id'=>'\d+'])]
-    public function details($id): Response
-    {
-        return $this->render('wish/details.html.twig', [
-            'id' => $id,
-        ]);
+
+    #[Route('/wish/DEBUG/jeuessai')]
+    public function debugJeuDEssai(EntityManagerInterface $entityManager): Response {
+        $wish = new Wish();
+        $wish->setTitle('Réussir une démonstration symfony sans faire d\'erreur')
+            ->setAuthor('Hervé')
+            ->setDateCreated(new \DateTime())
+            ->setIsPublish(true);
+        $entityManager->persist($wish);
+        $wish = new Wish();
+        $wish->setTitle('Donner une conférence au Devoxx')
+            ->setAuthor('Hervé')
+            ->setDateCreated(new \DateTime())
+            ->setIsPublish(true);
+        $entityManager->persist($wish);
+        $wish = new Wish();
+        $wish->setTitle('Faire le tour du monde à la voile')
+            ->setAuthor('Maxime')
+            ->setDateCreated(new \DateTime())
+            ->setIsPublish(true);
+        $entityManager->persist($wish);
+        $wish = new Wish();
+        $wish->setTitle('Rencontrer le président de la république')
+            ->setAuthor('Mélanie')
+            ->setDateCreated(new \DateTime())
+            ->setIsPublish(true);
+        $entityManager->persist($wish);
+        $entityManager->flush();
+        return $this->json('ok : Données ajoutées');
     }
 }
